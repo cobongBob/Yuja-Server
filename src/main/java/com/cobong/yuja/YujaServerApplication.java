88c8978fd6c640a29133430607a94648615bfc5e
Jongmin <dbswww@naver.com>
2021-04-25 23:04:05 +0900

    First Commit

---
+++ src/main/java/com/cobong/yuja/YujaServerApplication.java
@@ -0,0 +1,13 @@
+package com.cobong.yuja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YujaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YujaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cobong/yuja/YujaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cobong.yuja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YujaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
