c885f74a9119b9c5fc1c23661449c23c8423f87b
Jonghyun John Yoo <jonghyun.yoo0323@gmail.com>
2021-04-27 19:16:40 +0900

    User getall added

--- src/main/java/com/cobong/yuja/service/UserService.java
+++ src/main/java/com/cobong/yuja/service/UserService.java
@@ -1,10 +1,15 @@
 package com.cobong.yuja.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.cobong.yuja.model.User;
 import com.cobong.yuja.payload.request.UserSaveRequestDto;
+import com.cobong.yuja.payload.response.UserListResponseDto;
 import com.cobong.yuja.payload.response.UserResponseDto;
 import com.cobong.yuja.repository.UserRepository;
 
@@ -19,22 +24,17 @@ public class UserService {
 	public User userSave(UserSaveRequestDto dto) {
 		return userRepository.save(dto.dtoToEntity());
 	}
-
-	/*
-	@Transactional(readOnly = true)
-	public List<User> findAllUser(){
-		return userRepository.findAll().stream().map(null);
-	}	  
-	  */
+	
 	public UserResponseDto findById(Long id) {
 		User user = userRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("해당 유저를 찾을수 없습니다."));
 		UserResponseDto dto = new UserResponseDto().entityToDto(user);
 		return dto;
 	}
 	
-//	@Transactional(readOnly = true)
-//	public List<UserResponseDto> findAll(){
-//		return userRepository.findAll().stream().map(UserListResponseDto::new)
-//                .collect(Collectors.toList());
-//	}
+	@Transactional(readOnly = true)
+	public List<UserResponseDto> findAll(){
+		List<UserResponseDto> users = new ArrayList<UserResponseDto>();
+		userRepository.findAll().forEach(e -> users.add(new UserResponseDto().entityToDto(e)));
+		return users;
+	}
 }
