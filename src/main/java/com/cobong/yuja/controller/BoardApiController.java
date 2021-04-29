f2ada9a90f237d9924bf07368ddc9257d805f39f
Hyunil Suk <74586346+sukhyunil@users.noreply.github.com>
2021-04-29 13:19:14 +0900

    board, user controller ResponseEntity<없앰>

--- src/main/java/com/cobong/yuja/controller/BoardApiController.java
+++ src/main/java/com/cobong/yuja/controller/BoardApiController.java
@@ -35,31 +35,31 @@ public class BoardApiController {
 	@PostMapping("/api/board")
 	public ResponseEntity<?> insertBoard(@RequestBody BoardSaveRequestDto dto) {
 		System.out.println("==========>"+dto);
-		return new ResponseEntity<Board>(boardService.save(dto),HttpStatus.CREATED);
+		return new ResponseEntity<>(boardService.save(dto),HttpStatus.CREATED);
 	}
 	
 //	@GetMapping("/api/{boardCode}/board/{bno}")
 	@GetMapping("/{bno}")
 	public ResponseEntity<?> getOneBoard(@PathVariable Long bno) {
-		return new ResponseEntity<BoardResponseDto>(boardService.findById(bno),HttpStatus.OK);
+		return new ResponseEntity<>(boardService.findById(bno),HttpStatus.OK);
 	}
 	
 //	@GetMapping("/api/{boardCode}/board")
 	@GetMapping("/api/board")
 	public ResponseEntity<?> getAllBoard(){
-		return new ResponseEntity<List<BoardResponseDto>>(boardService.findAll(),HttpStatus.OK);
+		return new ResponseEntity<>(boardService.findAll(),HttpStatus.OK);
 	}
 	
 //	@PutMapping("/api/{boardCode}/board/{bno}")
 	@PutMapping("/{bno}")
 	public ResponseEntity<?> modifyBoard(@PathVariable Long bno, @RequestBody BoardUpdateRequestDto boardUpdateRequestDto){
-		return new ResponseEntity<BoardResponseDto>(boardService.modify(bno,boardUpdateRequestDto),HttpStatus.OK);
+		return new ResponseEntity<>(boardService.modify(bno,boardUpdateRequestDto),HttpStatus.OK);
 	}
 	
 //  @DeleteMapping("/api/{boardCode}/board/{bno}")
 	@DeleteMapping("/{bno}")
 	public ResponseEntity<?> deleteBoard(@PathVariable Long bno){
-		return new ResponseEntity<String>(boardService.delete(bno),HttpStatus.OK);
+		return new ResponseEntity<>(boardService.delete(bno),HttpStatus.OK);
 	}
 	
 	@GetMapping("/api/board/{boardCode}")
@@ -68,7 +68,7 @@ public class BoardApiController {
 		 * 선택한 게시판(유튜버,편집자, 썸네일러)의 글을 다 떙겨오는 컨트롤러
 		 * 이름이 좀 어색하당.. 좋은 이름 있음 바꿔주심 좋을 듯 
 		 */
-		return new ResponseEntity<List<BoardResponseDto>>(boardService.boardsInBoardType(boardCode), HttpStatus.OK);
+		return new ResponseEntity<>(boardService.boardsInBoardType(boardCode), HttpStatus.OK);
 	}
 	
 	@GetMapping("/api/userboard/{userId}")
@@ -76,7 +76,7 @@ public class BoardApiController {
 		/***
 		 * 매핑 받는 주소가 많이 걱정된다. 일단 하나의 보드만을 가져오는 친구와 차별을 두기위해 api를 추가했음.
 		 */
-		return new ResponseEntity<List<BoardResponseDto>>(boardService.boardsUserWrote(userId), HttpStatus.OK);
+		return new ResponseEntity<>(boardService.boardsUserWrote(userId), HttpStatus.OK);
 	}
 	
 	@GetMapping("/api/likedBy/{userId}")
@@ -84,11 +84,11 @@ public class BoardApiController {
 		/***
 		 *  유저가 찜/좋아요 한 게스글을 전부 불러오는 컨트롤러
 		 */
-		return new ResponseEntity<List<BoardResponseDto>>(boardService.boardsUserLiked(userId), HttpStatus.OK);
+		return new ResponseEntity<>(boardService.boardsUserLiked(userId), HttpStatus.OK);
 	}
 	
 	@GetMapping("/api/commentedBy/{userId}")
 	public ResponseEntity<?> boardsUserCommented(@PathVariable Long userId){
-		return new ResponseEntity<List<BoardResponseDto>>(boardService.boardsUserCommented(userId), HttpStatus.OK);
+		return new ResponseEntity<>(boardService.boardsUserCommented(userId), HttpStatus.OK);
 	}
 }
