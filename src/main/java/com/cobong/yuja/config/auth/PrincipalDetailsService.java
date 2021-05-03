f92a0c50a17da3301e26a2cf98b8a99db76bf86f
sukhyunil@naver.com <sukhyunil@naver.com>
2021-05-03 20:59:12 +0900

    AuthorityNames values 수정, jwt 필터, 생성 클래스  principaldetails + service 추가 securtity 폴더

--- src/main/java/com/cobong/yuja/config/auth/PrincipalDetailsService.java
+++ src/main/java/com/cobong/yuja/config/auth/PrincipalDetailsService.java
@@ -1,5 +1,35 @@
 package com.cobong.yuja.config.auth;
 
-public class PrincipalDetailsService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.cobong.yuja.model.User;
+import com.cobong.yuja.repository.user.UserRepository;
+
+public class PrincipalDetailsService implements UserDetailsService {
+	
+	@Autowired
+	UserRepository userRepository;
+
+	
+	@Transactional
+	public PrincipalDetails loadUserById(Long userId) {
+		User user = userRepository.findById(userId)
+				.orElseThrow(()-> new UsernameNotFoundException("not found with userId : " + userId));
+		
+		return PrincipalDetails.create(user);
+	}
+
+	// 로그인 
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		 User user = userRepository.findByUsername(username)
+				 .orElseThrow(()-> new UsernameNotFoundException("not found with username " + username ));
+		 
+		return PrincipalDetails.create(user);
+	}
 
 }
--- src/main/java/com/cobong/yuja/security/JwtTokenProvider.java
+++ src/main/java/com/cobong/yuja/security/JwtTokenProvider.java
@@ -1,75 +1,85 @@
-//package com.cobong.yuja.security;
-//
-//import java.util.Date;
-//
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.security.core.Authentication;
-//import org.springframework.stereotype.Component;
-//
-//import com.cobong.yuja.config.auth.PrincipalDetails;
-//
-//import io.jsonwebtoken.Claims;
-//import io.jsonwebtoken.ExpiredJwtException;
-//import io.jsonwebtoken.Jwts;
-//import io.jsonwebtoken.MalformedJwtException;
-//import io.jsonwebtoken.SignatureAlgorithm;
-//import io.jsonwebtoken.SignatureException;
-//import io.jsonwebtoken.UnsupportedJwtException;
-//
-//// 로그인 한 후 JWT를 생성하고, JWT 유효성을 검사파트
-//@Component
-//public class JwtTokenProvider {
-//
-//	private static final Logger logger = LoggerFactory.getLogger(JwtTokenProvider.class);
-//
-//	// (yml에서 설정함)
-//	@Value("${app.jwtSecret}")
-//	private String jwtSecret;
-//
-//	@Value("${app.jwtExpirationInMs}")
-//	private int jwtExpirationInMs;
-//
-//	// JWT 생성
-//	public String generateToken(Authentication authentication) {
-//
-//		PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
-//
-//		Date now = new Date();
-//		Date expirydate = new Date(now.getTime() + jwtExpirationInMs);
-//
-//		return Jwts.builder().setSubject(Long.toString(principalDetails.getUserid())) //
-//				.setIssuedAt(new Date())
-//				.setExpiration(expirydate)
-//				.signWith(SignatureAlgorithm.HS512, jwtSecret) 
-//				.compact(); 
-//	}
-//
-//	// JWT로 부터 UserId 가져옴(filter에서 토큰 유효성 검사용)
-//	public Long getUserIdFromJWT(String token) {
-//		Claims claims = Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody();
-//
-//		return Long.parseLong(claims.getSubject());
-//	}
-//
-//	// 유효성 검사
-//	public boolean validateToken(String authToken) {
-//
-//		try {
-//			Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
-//			return true;
-//		} catch (SignatureException ex) {
-//			logger.error("claims에서ㅏ오류로 검증이 실패");
-//		} catch (MalformedJwtException ex) {
-//			logger.error("구조적인 문제가 있는 JWToken");
-//		} catch (ExpiredJwtException ex) {
-//			logger.error("유효기간 지난 JWToken");
-//		} catch (UnsupportedJwtException ex) {
-//			logger.error("Unsupported JWToken");
-//		} catch (IllegalArgumentException ex) {
-//			logger.error("JWT claims string is empty.");
-//		}
-//		return false;
-//	}
-//}
+package com.cobong.yuja.security;
+
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import com.cobong.yuja.config.auth.PrincipalDetails;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SignatureException;
+import io.jsonwebtoken.UnsupportedJwtException;
+
+// 로그인 한 후 JWT를 생성하고, JWT 유효성을 검사파트
+@Component
+public class JwtTokenProvider {
+
+	private static final Logger logger = LoggerFactory.getLogger(JwtTokenProvider.class);
+
+	// apc.yml에 설정함
+	@Value("${app.jwtSecret}")
+	private String jwtSecret;
+
+	// +7일로 해둠
+	@Value("${app.jwtExpirationInMs}")
+	private int jwtExpirationInMs;
+
+	// JWT 생성 파트
+	public String generateToken(Authentication authentication) {
+
+		// getPrincipal PrincipalDetails담겨있는 정보를 가져옴
+		PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
+
+		Date now = new Date();
+		Date expirydate = new Date(now.getTime() + jwtExpirationInMs);
+
+		return Jwts.builder()
+				.setSubject(Long.toString(principalDetails.getUserId())) // sub에서 UserId를 구분
+				.setIssuedAt(new Date())
+				.setExpiration(expirydate) // 위 세개 PAYLOAD 
+				.signWith(SignatureAlgorithm.HS512, jwtSecret) // HEADER
+				.compact(); // 만듬
+	}
+
+	// JWT로 부터 UserId 가져옴(filter에서 토큰 유효성 검사용 디코딩으로 filter에서 check 할거임)
+	public Long getUserIdFromJWT(String token) {
+		Claims claims = Jwts
+				.parser()
+				.setSigningKey(jwtSecret)
+				.parseClaimsJws(token)
+				.getBody();
+
+		return Long.parseLong(claims.getSubject());
+	}
+
+	// 유효성 검사 파트
+	// 헤더에서온 비암호화된 암호 파싱
+	public boolean validateToken(String authToken) {
+
+		try {
+			Jwts.parser()
+			.setSigningKey(jwtSecret)
+			.parseClaimsJws(authToken);
+			return true;
+		} catch (SignatureException ex) {
+			logger.error("claims에서ㅏ오류로 검증이 실패");
+		} catch (MalformedJwtException ex) {
+			logger.error("구조적인 문제가 있는 JWToken");
+		} catch (ExpiredJwtException ex) {
+			logger.error("유효기간 지난 JWToken");
+		} catch (UnsupportedJwtException ex) {
+			logger.error("Unsupported JWToken");
+		} catch (IllegalArgumentException ex) {
+			logger.error("JWT claims string is empty.");
+		}
+		return false;
+	}
+}
