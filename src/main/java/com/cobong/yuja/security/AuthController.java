73c7d9935c62077413bdd58f053105b42fe60a1d
Hyunil Suk <74586346+sukhyunil@users.noreply.github.com>
2021-05-05 18:08:30 +0900

    authcontroller demo, 종현이형 brunch pull

--- src/main/java/com/cobong/yuja/security/AuthController.java
+++ src/main/java/com/cobong/yuja/security/AuthController.java
@@ -1,33 +1,37 @@
 package com.cobong.yuja.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import javax.validation.Valid;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.cobong.yuja.repository.user.AuthoritiesRepository;
-import com.cobong.yuja.repository.user.UserRepository;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/auth") // 설정해야댐
+@RequiredArgsConstructor
 public class AuthController {
-	
+
 	// filter에서 유저내용을 UsernamePasswordToken으로 담은게 여기에 이씀
 	// 이거 사용할려고 config에서 빈 인젝트
-	@Autowired
-	AuthenticationManager authenticationManager;
-	
-	@Autowired
-	UserRepository userRepository;
-	
-	@Autowired
-	AuthoritiesRepository authoritiesRepository;
-	
-	@Autowired
-	PasswordEncoder passwordEncoder;
-	
-	@Autowired
-	JwtTokenProvider jwtTokenProvider;
+	private final AuthenticationManager authenticationManager;
 
+	private final JwtTokenProvider jwtTokenProvider;
+
+	@PostMapping("/signin")
+	public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+		Authentication authentication = authenticationManager.authenticate(
+				new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		String jwt = jwtTokenProvider.generateToken(authentication);
+		return ResponseEntity.ok(new JwtAuthenticationResponse(jwt));
+	}
 }
