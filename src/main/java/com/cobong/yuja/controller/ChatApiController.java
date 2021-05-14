36762ef12f79ef55c36b85f658f18ea9a734cbf7
Jonghyun John Yoo <jonghyun.yoo0323@gmail.com>
2021-05-14 20:52:57 +0900

    05/14 night

--- src/main/java/com/cobong/yuja/controller/ChatApiController.java
+++ src/main/java/com/cobong/yuja/controller/ChatApiController.java
@@ -1,31 +1,17 @@
 package com.cobong.yuja.controller;
 
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 
-import lombok.RequiredArgsConstructor;
+import com.cobong.yuja.config.websocket.SocketMessage;
 
-@RestController
-@RequiredArgsConstructor
+@Controller
 public class ChatApiController {
-	private final SimpMessagingTemplate webSocket;
-	
-	@MessageMapping("/sendTo") 
-	@SendTo("/topics/sendTo") 
-	public String SendToMessage() throws Exception { 
-		return "SendTo"; 
+	@MessageMapping("/user-all") 
+	@SendTo("/topic/user") 
+	public SocketMessage send(@Payload SocketMessage msg){ 
+		return msg; 
 	} 
-	
-	@MessageMapping("/Template") 
-	public void SendTemplateMessage() { 
-		webSocket.convertAndSend("/topics/template" , "Template"); 
-	} 
-	
-	@RequestMapping(value="/api") 
-	public void SendAPI() { 
-		webSocket.convertAndSend("/topics/api" , "API"); 
-	}
 }
