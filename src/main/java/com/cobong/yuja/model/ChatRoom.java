1e69eca473de320737ccfa28dda48851c83a646d
Jonghyun John Yoo <jonghyun.yoo0323@gmail.com>
2021-05-17 22:47:53 +0900

    entities for socket commented

--- src/main/java/com/cobong/yuja/model/ChatRoom.java
+++ src/main/java/com/cobong/yuja/model/ChatRoom.java
@@ -1,23 +1,23 @@
-package com.cobong.yuja.model;
-
-import java.util.List;
-
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-
-import lombok.Getter;
-
-@Entity
-@Getter
-public class ChatRoom {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long roomId;
-	
-	@OneToMany(mappedBy = "chatRoom", cascade = CascadeType.REMOVE)
-	private List<SocketMessage> messages;
-}
+//package com.cobong.yuja.model;
+//
+//import java.util.List;
+//
+//import javax.persistence.CascadeType;
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+//import javax.persistence.OneToMany;
+//
+//import lombok.Getter;
+//
+//@Entity
+//@Getter
+//public class ChatRoom {
+//	@Id
+//	@GeneratedValue(strategy = GenerationType.IDENTITY)
+//	private Long roomId;
+//	
+//	@OneToMany(mappedBy = "chatRoom", cascade = CascadeType.REMOVE)
+//	private List<SocketMessage> messages;
+//}
