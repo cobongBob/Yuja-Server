f0a2e35c3e0477f693654ab16cfa3713cb866661
bit <bit@DESKTOP-C7825S8>
2021-05-31 20:52:20 +0900

    데스크탑 교체 후 커밋

--- src/main/java/com/cobong/yuja/repository/refreshToken/RefreshTokenRepository.java
+++ src/main/java/com/cobong/yuja/repository/refreshToken/RefreshTokenRepository.java
@@ -5,7 +5,7 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.cobong.yuja.model.RefreshToken;
-
+  
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
     Optional<RefreshToken> findByUserId(Long userId);
     Optional<RefreshToken> deleteByUserId(Long userId);
